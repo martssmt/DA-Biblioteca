1c8ced659015bb6041f018cf1f2e90daaed11c1f
martssmt <lozanomarcosmarta@gmail.com>
2025-01-12 02:54:03 +0100

    Utilidades. Funciones añadidas

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -19,7 +19,7 @@ public class Utilidades {
                 if (resp < min || resp > max) System.out.println("Número fuera del rango. Vuelva a introducirlo.");
             } catch (InputMismatchException ex) {
                 sc.nextLine();
-                System.out.println("Entrada no válida. Vuelva a intentarlo.");
+                System.out.println("Entrada no válida. Vuelva a intentarlo.\n");
                 resp=min-1;
             }
         } while (resp<min||resp>max);
@@ -33,7 +33,7 @@ public class Utilidades {
             System.out.print(mensaje+ "('S' para SÍ, 'N' para NO): ");
             m=sc.nextLine();
             if (!m.equalsIgnoreCase("S")&&!m.equalsIgnoreCase("N")) {
-                System.out.println("Cadena no reconocida como posible respuesta. Vuelva a introducirla.");
+                System.out.println("Cadena no reconocida como posible respuesta. Vuelva a introducirla.\n");
             }
         } while (!m.equalsIgnoreCase("S")&&!m.equalsIgnoreCase("N"));
         if (m.equalsIgnoreCase("S")) resp=true; // TRUE será la respuesta 'SI'
@@ -55,10 +55,73 @@ public class Utilidades {
                 if (resp<0) System.out.println("Número fuera del rango. Vuelva a introducirlo.");
             } catch (InputMismatchException ex) {
                 sc.nextLine();
-                System.out.println("Entrada no válida. Vuelva a intentarlo.");
+                System.out.println("Entrada no válida. Vuelva a intentarlo.\n");
                 resp=-1;
             }
         } while (resp<0);
+        return resp;
+    }
+
+    public static String leerFecha(String mensaje) {
+        boolean correcto=false;
+        String resp="";
+        do {
+            System.out.print(mensaje);
+            resp=sc.nextLine();
+            String[] comprobar=resp.split("/");
+            if (comprobar.length!=3) {
+                try {
+                    int dia = Integer.parseInt(comprobar[0]);
+                    int mes = Integer.parseInt(comprobar[1]);
+                    int anio = Integer.parseInt(comprobar[2]);
+                    if (mes >= 1 && mes <= 12) {
+                        switch (mes) {
+                            case 1, 3, 5, 7, 8, 10, 12:
+                                if (dia >= 1 && dia <= 31) correcto = true;
+                                break;
+                            case 4, 6, 9, 11:
+                                if (dia >= 1 && dia <= 30) correcto = true;
+                                break;
+                            case 2:
+                                if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) { // bisiesto
+                                    if (dia >= 1 && dia <= 29) correcto = true;
+                                } else {
+                                    if (dia >= 1 && dia <= 28) correcto = true;
+                                }
+                                break;
+                        }
+                    }
+                } catch (NumberFormatException _) {}
+            }
+            if (!correcto) System.out.println("Fecha incorrecta. Vuelva a intentarlo\n");
+        } while (!correcto);
+        return resp;
+    }
+
+    public static String leerMatricula(String mensaje) {
+        boolean correcto=false;
+        String resp="";
+        do {
+            System.out.print(mensaje);
+            resp=sc.nextLine();
+            if (resp.matches("[a-zA-Z]{2}\\d{4}")) {  // Dos letras seguidas de cuatro dígitos
+                correcto = true;
+            } else {
+                System.out.println("Matrícula incorrecta. Vuélvalo a intentar.\n");
+            }
+        } while (!correcto);
+        return resp;
+    }
+
+    public static String leerTel(String mensaje) {
+        boolean correcto=false;
+        String resp="";
+        do {
+            System.out.print(mensaje);
+            resp=sc.nextLine();
+            if (resp.length()==9) correcto = true;
+            else System.out.println("Teléfono incorrecto. Vuélvalo a intentar.\n");
+        } while (!correcto);
         return resp;
     }
 
